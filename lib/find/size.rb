19d430e5f4cbb7d3eae5cb2c3d8e20f6071da1e1
Yotsumoto, Toshitaka <yotsumoto@qinuau.net>
2017-08-18 16:01:48 +0900

    Modified module to class.

--- lib/find/size.rb
+++ lib/find/size.rb
@@ -1,4 +1,4 @@
-module Find::Size
+class Find::Size
   class Result
     attr_accessor :path, :size
 
@@ -8,34 +8,36 @@ module Find::Size
     end
   end
 
-  def self.get_size(path, size_least = nil)
-    if (File.directory?(path))
-      size = 0
-      Dir.glob(path + '/**/*').each do |path_each|
-        begin
-          size += File.size(path_each)
-        rescue
+  class << Find::Size
+    def get_size(path, size_least = nil)
+      if (File.directory?(path))
+        size = 0
+        Dir.glob(path + '/**/*').each do |path_each|
+          begin
+            size += File.size(path_each)
+          rescue
 
+          end
+        end
+      else
+        begin
+          size = File.stat(path).size
+        rescue
+          size = 0
         end
       end
-    else
-      begin
-        size = File.stat(path).size
-      rescue
-        size = 0
-      end
-    end
 
-    if size_least != nil
-      if size >= size_least
+      if size_least != nil
+        if size >= size_least
+          return return_result(path, size)
+        end
+      else
         return return_result(path, size)
       end
-    else
-      return return_result(path, size)
     end
-  end
 
-  def self.return_result(path, size)
-    return Find::Size::Result.new(path, size)
+    def return_result(path, size)
+      return Find::Size::Result.new(path, size)
+    end
   end
 end
